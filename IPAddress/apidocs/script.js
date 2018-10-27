019b50151581abc73fdce694426c25dbca2ad76c
Sean Foley <scfoley@us.ibm.com>
2018-10-27 02:16:07 -0400

    updated javadocs

--- IPAddress/apidocs/script.js
+++ IPAddress/apidocs/script.js
@@ -1,14 +1,3 @@
-/*========================================================================
- *  Licensed Materials - Property of IBM
- *  "Restricted Materials of IBM"
- *
- *  IBM SDK, Java(tm) Technology Edition, v8
- *  (C) Copyright IBM Corp. 2000, 2014. All Rights Reserved
- *
- *  US Government Users Restricted Rights - Use, duplication or disclosure
- *  restricted by GSA ADP Schedule Contract with IBM Corp.
- *========================================================================
- */
 function show(type)
 {
     count = 0;
